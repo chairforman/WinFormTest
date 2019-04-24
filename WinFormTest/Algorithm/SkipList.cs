b82b50ee3efd63c72930a7bfe372068e32e4c143
hujy <hujy@yonxin.com>
2019-04-24 17:49:23 +0800

    跳表实现

---
+++ WinFormTest/Algorithm/SkipList.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Text;
+
+namespace WinFormTest.Algorithm
+{
+    /// <summary>
+    /// 跳表
+    /// </summary>
+    public class SkipList
+    {
+        /// <summary>
+        /// 最大层数
+        /// </summary>
+        private static int MAX_LEVEL = 16;
+
+        /// <summary>
+        /// 当前总层数
+        /// </summary>
+        private int levelCount = 1;
+
+        /// <summary>
+        /// 头节点
+        /// </summary>
+        private Node head = new Node();
+
+        private Random r = new Random();
+
+        #region 构造函数
+        public SkipList() { }
+
+        public SkipList(int maxLevel)
+        {
+            MAX_LEVEL = maxLevel;
+        }
+        #endregion
+
+        /// <summary>
+        /// 查找指定值的节点
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Node Find(int value)
+        {
+            Node p = head;
+            for (int i = levelCount - 1; i >= 0; i--)
+            {
+                while (p.forward[i] != null && p.forward[i].Data < value)
+                {
+                    p = p.forward[i];
+                }
+            }
+
+            if (p.forward[0] != null && p.forward[0].Data == value)
+            {
+                return p.forward[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Insert(int value)
+        {
+            int level = RandomLevel();//新节点总层数
+            Node newNode = new Node(value, level);
+            Node[] update = new Node[level];
+            for (int i = level - 1; i >= 0; i--)
+            {
+                Node p = head;
+                while (p.forward[i] != null && p.forward[i].Data < value)
+                {
+                    p = p.forward[i];
+                }
+                update[i] = p;
+            }
+
+            for (int i = 0; i < update.Length; i++)
+            {
+                newNode.forward[i] = update[i].forward[i];
+                update[i].forward[i] = newNode;
+            }
+
+            if (levelCount < level)
+            {
+                levelCount = level;
+            }
+        }
+
+        public void Delete(int value)
+        {
+            Node[] update = new Node[levelCount];
+            Node p = head;
+            for (int i = levelCount - 1; i >= 0; i--)
+            {
+                while (p.forward[i] != null && p.forward[i].Data < value)
+                {
+                    p = p.forward[i];
+                }
+                update[i] = p;
+            }
+
+            if (p.forward[0] != null && p.forward[0].Data == value)
+            {
+                for (int i = 0; i < update.Length; i++)
+                {
+                    if (update[i].forward[i] != null && update[i].forward[i].Data == value)
+                    {
+                        update[i].forward[i] = update[i].forward[i].forward[i];
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 输出全部节点
+        /// </summary>
+        /// <returns></returns>
+        public string PrintAll()
+        {
+            Node p = head;
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                sb.Append(p.ToString() + " ");
+                p = p.forward[0];
+            } while (p != null);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 随机生成节点的总层数
+        /// </summary>
+        /// <returns></returns>
+        private int RandomLevel()
+        {
+            int ret = 1;
+            for (int i = 1; i < MAX_LEVEL; i++)
+            {
+                if (r.Next() % 2 == 1)
+                {
+                    ret++;
+                }
+            }
+            return ret;
+        }
+
+        public class Node
+        {
+            private int _data = -1;//初始值
+            public int Data
+            {
+                get { return _data; }
+            }
+
+            /// <summary>
+            /// 当前节点最高层级
+            /// </summary>
+            private int _maxLevel = 0;
+
+            /// <summary>
+            /// 当前节点在每一层里指向的下一个节点
+            /// </summary>
+            public Node[] forward = new Node[MAX_LEVEL];
+
+            #region 构造函数
+            public Node() { }
+
+            public Node(int data)
+            {
+                _data = data;
+            }
+
+            public Node(int data, int maxLevel)
+            {
+                _data = data;
+                _maxLevel = maxLevel;
+            }
+            #endregion
+
+            public override string ToString()
+            {
+                return $"{{ data: {_data}; levels: {_maxLevel} }}";
+            }
+        }
+    }
+}
--- WinFormTest/Form1.cs
+++ WinFormTest/Form1.cs
@@ -20,21 +20,43 @@ namespace WinFormTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int[] a = { 3, 8, 5, 1, 9, 4, 3, 8, 7, 6, 2, 0 };
+            #region 算法
+            //int[] a = { 3, 8, 5, 1, 9, 4, 3, 8, 7, 6, 2, 0 };
 
-            QuickSort.Sort(a);
-            for (int i = 0; i < a.Length; i++)
-            {
-                textBox1.Text += a[i] + " ";
-            }
+            //QuickSort.Sort(a);
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    textBox1.Text += a[i] + " ";
+            //}
 
-            textBox1.Text += "\r\n";
-            
-            int[] result = MergeSort.Sort(a);
-            for (int i = 0; i < result.Length; i++)
-            {
-                textBox1.Text += result[i] + " ";
-            }
+            //textBox1.Text += "\r\n";
+
+            //int[] result = MergeSort.Sort(a);
+            //for (int i = 0; i < result.Length; i++)
+            //{
+            //    textBox1.Text += result[i] + " ";
+            //}
+
+            var sl = new SkipList(4);
+            sl.Insert(1);
+            sl.Insert(10);
+            sl.Insert(2);
+            sl.Insert(9);
+            sl.Insert(3);
+            sl.Insert(8);
+            sl.Insert(4);
+            sl.Insert(7);
+            sl.Insert(5);
+            sl.Insert(6);
+            textBox1.Text = sl.PrintAll();
+
+            var node = sl.Find(6);
+            textBox1.Text += "\r\n" +  node.ToString();
+
+            sl.Delete(6);
+            textBox1.Text += "\r\n" + sl.PrintAll();
+
+            #endregion
         }
     }
 }
