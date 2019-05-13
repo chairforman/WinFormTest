94fc5f255cf383d218382de5cf7c703cb57029ac
hujy <hujy@yonxin.com>
2019-05-13 16:41:52 +0800

    二叉树查找、插入、删除

--- WinFormTest/Algorithm/BinaryTree.cs
+++ WinFormTest/Algorithm/BinaryTree.cs
@@ -144,5 +144,188 @@ namespace WinFormTest.Algorithm
 
             return ret.ToString();
         }
+
+        /// <summary>
+        /// 二叉树查找节点
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static Node<T> Find<T>(Node<T> root, T data)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            Node<T> node = root;
+            while (node != null)
+            {
+                if (node.Data.Equals(data))
+                {
+                    return node;
+                }
+
+                if (data is int)
+                {
+                    int? dInt = data as int?;
+                    int? nInt = node.Data as int?;
+                    if (dInt < nInt)
+                    {
+                        node = node.Left;
+                    }
+                    else
+                    {
+                        node = node.Right;
+                    }
+                }
+                else
+                {
+                    Node<T> lNode = null, rNode = null;
+                    if (node.Left != null)
+                    {
+                        lNode = Find(node.Left, data);
+                    }
+                    if (lNode == null && node.Right != null)
+                    {
+                        rNode = Find(node.Right, data);
+                    }
+
+                    if (lNode != null)
+                    {
+                        return lNode;
+                    }
+                    else if (rNode != null)
+                    {
+                        return rNode;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 二叉树插入节点
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static Node<int> Insert(Node<int> root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node<int>(data);
+                return null;
+            }
+
+            Node<int> p = root;
+            while(p != null)
+            {
+                if (p.Data > data)
+                {
+                    if (p.Left == null)
+                    {
+                        p.Left = new Node<int>(data);
+                        return p.Left;
+                    }
+                    p = p.Left;
+                }
+                else if (p.Data < data)
+                {
+                    if (p.Right == null)
+                    {
+                        p.Right = new Node<int>(data);
+                        return p.Right;
+                    }
+                    p = p.Right;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 二叉树删除节点
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static bool Delete(Node<int> root, int data)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            Node<int> nodeParent = null;
+            Node<int> node = root;
+            while (node != null && node.Data != data)
+            {
+                nodeParent = node;
+                if (node.Data > data)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    node = node.Right;
+                }
+            }
+            if (node == null)
+            {
+                return false;//没有找到
+            }
+
+            //待删节点有左右子节点
+            if (node.Left != null && node.Right != null)
+            {
+                Node<int> minNodeParent = node;
+                Node<int> minNode = node.Right;
+                //取右分支的最小节点
+                while(minNode.Left != null)
+                {
+                    minNodeParent = minNode;
+                    minNode = minNode.Left;
+                }
+                //右分支最小节点替换待删节点
+                node.Data = minNode.Data;
+                //右分支最小节点变成待删节点
+                nodeParent = minNodeParent;
+                node = minNode;
+            }
+
+            Node<int> nodeChild = null;
+            if (node.Left != null)
+            {
+                nodeChild = node.Left;
+            }
+            else if (node.Right != null)
+            {
+                nodeChild = node.Right; 
+            }
+
+            if (nodeParent == null)
+            {
+                root = null;
+                return true;
+            }
+            else if (nodeParent.Left == node)
+            {
+                nodeParent.Left = nodeChild;
+                return true;
+            }
+            else if (nodeParent.Right == node)
+            {
+                nodeParent.Right = nodeChild;
+                return true;
+            }
+
+            return false;
+        }
     }
 }
--- WinFormTest/Form1.cs
+++ WinFormTest/Form1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,12 +24,18 @@ namespace WinFormTest
         private void button1_Click(object sender, EventArgs e)
         {
             #region CYQ
-            //string sql = textBox1.Text;
+            //string sql = "BPMS_Organization";
             //try
             //{
-            //    using (MAction action = new MAction(sql))
-            //    {
 
+            //    using (MAction action = new MAction(sql, "Conn"))
+            //    {
+            //        action.Data.Clear();
+            //    }
+
+            //    using (MAction action = new MAction(sql, "OldASConn"))
+            //    {
+            //        action.Data.Clear();
             //    }
             //}
             //catch (Exception ex)
@@ -78,24 +86,97 @@ namespace WinFormTest
             #endregion
 
             #region 二叉树
-            BinaryTree.Node<string> an = new BinaryTree.Node<string>("A");
-            BinaryTree.Node<string> bn = new BinaryTree.Node<string>("B");
-            BinaryTree.Node<string> cn = new BinaryTree.Node<string>("C");
-            BinaryTree.Node<string> dn = new BinaryTree.Node<string>("D");
-            BinaryTree.Node<string> en = new BinaryTree.Node<string>("E");
-            BinaryTree.Node<string> fn = new BinaryTree.Node<string>("F");
-            BinaryTree.Node<string> gn = new BinaryTree.Node<string>("G");
-            an.Left = bn;
-            an.Right = cn;
-            bn.Left = dn;
-            bn.Right = en;
-            cn.Left = fn;
-            cn.Right = gn;
+            //BinaryTree.Node<string> an = new BinaryTree.Node<string>("A");
+            //BinaryTree.Node<string> bn = new BinaryTree.Node<string>("B");
+            //BinaryTree.Node<string> cn = new BinaryTree.Node<string>("C");
+            //BinaryTree.Node<string> dn = new BinaryTree.Node<string>("D");
+            //BinaryTree.Node<string> en = new BinaryTree.Node<string>("E");
+            //BinaryTree.Node<string> fn = new BinaryTree.Node<string>("F");
+            //BinaryTree.Node<string> gn = new BinaryTree.Node<string>("G");
+            //an.Left = bn;
+            //an.Right = cn;
+            //bn.Left = dn;
+            //bn.Right = en;
+            //cn.Left = fn;
+            //cn.Right = gn;
 
-            textBox1.Text = BinaryTree.PreOrder(an) + "\r\n";
-            textBox1.Text += BinaryTree.InOrder(an) + "\r\n";
-            textBox1.Text += BinaryTree.PostOrder(an) + "\r\n";
-            textBox1.Text += BinaryTree.LevelOrder(an) + "\r\n";
+            //textBox1.Text = BinaryTree.PreOrder(an) + "\r\n";
+            //textBox1.Text += BinaryTree.InOrder(an) + "\r\n";
+            //textBox1.Text += BinaryTree.PostOrder(an) + "\r\n";
+            //textBox1.Text += BinaryTree.LevelOrder(an) + "\r\n";
+
+            //BinaryTree.Node<int> n1 = new BinaryTree.Node<int>(1);
+            //BinaryTree.Node<int> n2 = new BinaryTree.Node<int>(2);
+            //BinaryTree.Node<int> n3 = new BinaryTree.Node<int>(3);
+            //BinaryTree.Node<int> n4 = new BinaryTree.Node<int>(4);
+            //BinaryTree.Node<int> n5 = new BinaryTree.Node<int>(5);
+            //BinaryTree.Node<int> n6 = new BinaryTree.Node<int>(6);
+            //BinaryTree.Node<int> n7 = new BinaryTree.Node<int>(7);
+            //n1.Left = n2;
+            //n1.Right = n3;
+            //n2.Left = n4;
+            //n2.Right = n5;
+            //n3.Left = n6;
+            //n3.Right = n7;
+
+            //BinaryTree.Node<int> root = new BinaryTree.Node<int>(33);
+            //BinaryTree.Node<int> n17 = new BinaryTree.Node<int>(17);
+            //BinaryTree.Node<int> n50 = new BinaryTree.Node<int>(50);
+            //BinaryTree.Node<int> n13 = new BinaryTree.Node<int>(13);
+            //BinaryTree.Node<int> n18 = new BinaryTree.Node<int>(18);
+            //BinaryTree.Node<int> n34 = new BinaryTree.Node<int>(34);
+            //BinaryTree.Node<int> n58 = new BinaryTree.Node<int>(58);
+            //BinaryTree.Node<int> n16 = new BinaryTree.Node<int>(16);
+            //BinaryTree.Node<int> n25 = new BinaryTree.Node<int>(25);
+            //BinaryTree.Node<int> n51 = new BinaryTree.Node<int>(51);
+            //BinaryTree.Node<int> n66 = new BinaryTree.Node<int>(66);
+            //BinaryTree.Node<int> n19 = new BinaryTree.Node<int>(19);
+            //BinaryTree.Node<int> n27 = new BinaryTree.Node<int>(27);
+            //n33.Left = n17;
+            //n33.Right = n50;
+            //n17.Left = n13;
+            //n17.Right = n18;
+            //n50.Left = n34;
+            //n50.Right = n58;
+            //n13.Right = n16;
+            //n18.Right = n25;
+            //n58.Left = n51;
+            //n58.Right = n66;
+            //n25.Left = n19;
+            //n25.Right = n27;
+
+            //BinaryTree.Node<int> node = BinaryTree.Find(n33, 19);
+            //if (node  != null)
+            //{
+            //    textBox1.Text = node.Data.ToString();
+            //}
+            //else
+            //{
+            //    textBox1.Text = "null";
+            //}
+
+            BinaryTree.Node<int> root = new BinaryTree.Node<int>(33);
+            BinaryTree.Insert(root, 16);
+            BinaryTree.Insert(root, 50);
+            BinaryTree.Insert(root, 13);
+            BinaryTree.Insert(root, 18);
+            BinaryTree.Insert(root, 34);
+            BinaryTree.Insert(root, 58);
+            BinaryTree.Insert(root, 15);
+            BinaryTree.Insert(root, 17);
+            BinaryTree.Insert(root, 25);
+            BinaryTree.Insert(root, 51);
+            BinaryTree.Insert(root, 66);
+            BinaryTree.Insert(root, 19);
+            BinaryTree.Insert(root, 27);
+            BinaryTree.Insert(root, 55);
+            textBox1.Text += BinaryTree.LevelOrder(root) + "\r\n";
+
+            BinaryTree.Delete(root, 13);
+            BinaryTree.Delete(root, 18);
+            BinaryTree.Delete(root, 55);
+            textBox1.Text += BinaryTree.LevelOrder(root) + "\r\n";
+
             #endregion
 
             #endregion
